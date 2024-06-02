a28e3bd789583f3fb6e6cbf67186d1a47d013f1a
Manikanta Bandham <bandhammanikanta@gmail.com>
2024-06-02 15:05:01 -0400

    Errors in MainApp.jsx

--- src/MainApp.jsx
+++ src/MainApp.jsx
@@ -1,9 +1,5 @@
 import React, { useState, useEffect, Suspense } from 'react';
-<<<<<<< HEAD
-import { Routes, Route } from 'react-router-dom'; // Update imports
-=======
 import { Switch, Route } from 'react-router-dom';
->>>>>>> 257de97 (Initial Commit)
 import FallbackSpinner from './components/FallbackSpinner';
 import NavBarWithRouter from './components/NavBar';
 import Home from './components/Home';
@@ -25,31 +21,16 @@ function MainApp() {
     <div className="MainApp">
       <NavBarWithRouter />
       <main className="main">
-<<<<<<< HEAD
-        <Suspense fallback={<FallbackSpinner />}>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            {data && data.sections.map((route) => {
-=======
         <Switch>
           <Suspense fallback={<FallbackSpinner />}>
             <Route exact path="/" component={Home} />
             {data
               && data.sections.map((route) => {
->>>>>>> 257de97 (Initial Commit)
                 const SectionComponent = React.lazy(() => import('./components/' + route.component));
                 return (
                   <Route
                     key={route.headerTitle}
                     path={route.path}
-<<<<<<< HEAD
-                    element={<SectionComponent header={route.headerTitle} />}
-                  />
-                );
-              })}
-          </Routes>
-        </Suspense>
-=======
                     component={() => (
                       <SectionComponent header={route.headerTitle} />
                     )}
@@ -58,7 +39,6 @@ function MainApp() {
               })}
           </Suspense>
         </Switch>
->>>>>>> 257de97 (Initial Commit)
       </main>
     </div>
   );
